a1548a39cd5b3e9d01981be3baf5a7b74e5316b2
olegfateev13 <58751132+olegfateev13@users.noreply.github.com>
2023-12-27 20:52:30 +0300

    all task code style

--- homework/tasks/task_3.py
+++ homework/tasks/task_3.py
@@ -10,22 +10,6 @@ class Ticket:
 
 
 async def coroutines_execution_order(coros: list[Awaitable[Ticket]]) -> str:
-    # Необходимо выполнить все полученные корутины, затем упорядочить их результаты
-    # по полю number и вернуть строку, состоящую из склеенных полей key.
-    #
-    # Пример:
-    # r1 = Ticket(number=2, key='мыла')
-    # r2 = Ticket(number=1, key='мама')
-    # r3 = Ticket(number=3, key='раму')
-    #
-    # Результат: 'мамамылараму'
-    #
-    # YOUR CODE GOES HERE
     results = await asyncio.gather(*coros)
-    
-    # Сортируем результаты по полю number
     sorted_results = sorted(results, key=lambda ticket: ticket.number)
-    
-    # Склеиваем поля key и возвращаем результат
     return ''.join(ticket.key for ticket in sorted_results)
-
--- homework/tasks/task_5.py
+++ homework/tasks/task_5.py
@@ -3,13 +3,6 @@ from typing import Coroutine
 
 
 async def limit_execution_time(coro: Coroutine, max_execution_time: float) -> None:
-    # Функция принимает на вход корутину, которую необходимо запустить, однако иногда она выполняется
-    # слишком долго, это время необходимо ограничить переданным на вход количеством секунд.
-    #
-    # Тест проверяет, что каждая переданная корутина была запущена, и все они завершились за заданное
-    # время.
-    #
-    # YOUR CODE GOES HERE
     try:
         await asyncio.wait_for(coro, timeout=max_execution_time)
     except asyncio.TimeoutError:
@@ -17,11 +10,7 @@ async def limit_execution_time(coro: Coroutine, max_execution_time: float) -> No
 
 
 async def limit_execution_time_many(*coros: Coroutine, max_execution_time: float) -> None:
-    # Функция эквивалентна limit_execution_time, но корутин на вход приходит несколько.
-    #
-    # YOUR CODE GOES HERE
     try:
         await asyncio.wait_for(asyncio.gather(*coros), timeout=max_execution_time)
     except asyncio.TimeoutError:
         pass
-
